79b9cfb14f1e85a9d3bc9bc628e38dce59a1b2f9
satyam kaurav <satyam_031@outlook.com>
2024-05-23 14:15:30 +0530

    new updates

--- uidGen.js
+++ uidGen.js
@@ -2,53 +2,39 @@ function nextChar(c) {
   return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
-function flip_flop({ arr, lowestbound, highestbond }) {
-  const last_item = arr.length - 1;
-  console.log("b:", { arr, lowestbound, highestbond });
-  let isFliped = false;
-  for (let i in arr) {
-    if (arr[i] < highestbond) {
-      arr[i] = nextChar(arr[i]);
-      break;
-    } else if (arr[i] === highestbond) {
-      arr[i] = lowestbound;
-      if (parseInt(i) === last_item) isFliped = true;
-      continue;
+function flip_flop(uid_str) {
+  const [prefix, uid] = uid_str.split("#");
+  let uid_arr = Array.from(uid);
+  const uid_len = uid_arr.length;
+
+  for (let index = uid_len - 1; index >= 0; index--) {
+    const element = uid_arr[index];
+
+    if (element >= "A" && element <= "Z") {
+      if (element < "Z") {
+        const nxt = nextChar(element);
+        uid_arr[index] = nxt;
+        break;
+      } else if (element === "Z") {
+        uid_arr[index] = "A";
+        continue;
+      }
+    } else if (element >= "0" && element <= "9") {
+      if (element < "9") {
+        const nxt = nextChar(element);
+        uid_arr[index] = nxt;
+        break;
+      } else if (element === "9") {
+        uid_arr[index] = "0";
+        continue;
+      }
     }
   }
 
-  console.log("a:", arr);
-  return { arr, isFliped };
+  return prefix + "#" + uid_arr.join("");
 }
 
-function uid_incr(perv_uid, uid_for) {
-  const uid_tail = perv_uid.split("#")[1];
-  let tail_digits = uid_tail.substring(0, 4);
-  let tail_chars = uid_tail.substring(4, 7);
+const prev_uid = "parent#0000A9";
+const new_uid = flip_flop(prev_uid);
 
-  let char_arr = tail_chars.split("").reverse();
-
-  const { arr, isFliped } = flip_flop({
-    arr: char_arr,
-    lowestbound: "A",
-    highestbond: "Z",
-  });
-
-  tail_chars = arr.reverse().join("");
-
-  if (isFliped) {
-    let digit_arr = tail_digits.split("").reverse();
-    const { arr, isFliped } = flip_flop({
-      arr: digit_arr,
-      lowestbound: "0",
-      highestbond: "9",
-    });
-    tail_digits = arr.reverse().join("");
-  }
-
-  return `${uid_for}#${tail_digits}${tail_chars}`;
-}
-
-const new_uid = uid_incr("parent#0000ZZZ", "parent");
-
-console.log(new_uid);
+console.log(prev_uid, new_uid);
